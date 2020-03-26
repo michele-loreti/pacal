2007d872d0eb23911fc43a2efdd6083c5a49f8ef
loreti <michele.loreti@unicam.it>
2020-03-26 10:31:02 +0100

    First commit.

---
+++ src/main/java/it/unicam/cs/pa/pacal/CalcState.java
@@ -0,0 +1,53 @@
+package it.unicam.cs.pa.pacal;
+
+public class CalcState {
+
+    private double value1;
+    private double value2;
+    private double mem;
+    private boolean status;
+
+    public CalcState() {
+        this.status = true;
+        init();
+    }
+
+    private void init() {
+        this.value1 = 0.0;
+        this.value2 = 0.0;
+        this.mem = 0.0;
+    }
+
+    public double getValue1() {
+        return value1;
+    }
+
+    public double getValue2() {
+        return value2;
+    }
+
+    public double getMem( ) {
+        return mem;
+    }
+
+    public void store() {
+        this.mem = this.value1;
+    }
+
+    public void setValue(double value) {
+        this.value2 = value1;
+        this.value1 = value;
+    }
+
+    public void reset() {
+        init();
+    }
+
+    public boolean isOn() {
+        return status;
+    }
+
+    public void turnOff() {
+        this.status = false;
+    }
+}
